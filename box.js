f8e10ed662d4a119aa5eada08ef2dfa727ab5e1d
Parvin Sattorova <nanajon66@gmail.com>
2023-07-04 13:02:31 -0600

    feat: Add test cases and implimetation

--- box.js
+++ box.js
@@ -1,4 +1,60 @@
+// function createBox(height, width) {
+//   var box = {
+//     height: height || 100,
+//     width: width || 100
+//     }
+//   return box
+// };
+  
+function createBox(height = 100, width = 100) {
+  return { height, width };
+};
+  
+function calculateArea(box) {
+    return box.height * box.width;;  
+};
 
+// function increaseWidth(box, value) {
+//     return { ...box, width: box.width + value };
+// };
+  
+//   function increaseHeight(box, value) {
+//     return { ...box, height: box.height + value };
+// };
+  
+  //The { ...box, height: box.height + value } syntax is part of modern JavaScript (often called ES6 or ES2015), not specific to any library or framework like React. This pattern is commonly used in JavaScript applications to maintain immutability, i.e., not modifying existing objects, but creating new ones with updated properties.
+
+// ...box: This is called the spread operator. It's used here to copy all properties from the box object into a new object.
+
+// height: box.height + value: This is creating or updating the height property in the new object. It takes the current height value of the box object and adds the value.
+
+// The result is a new object with all the same properties as the original box object, but with an updated height value. This leaves the original box object unchanged.
+
+// For example, if you have:
+
+// let box = { height: 50, width: 70 };
+// let value = 10;
+
+//Then, this line:
+
+
+// let newBox = { ...box, height: box.height + value };
+
+// Would result in newBox being a new object with the value { height: 60, width: 70 }, while the original box object remains unchanged ({ height: 50, width: 70 }).
+
+function increment(box, value, dimension) {
+    return { ...box, [dimension]: box[dimension] + value };
+};
+
+//The increment function takes in a box, a value, and a dimension. The dimension argument should be a string, either "width" or "height". The function creates a new object with all the properties of the box object, but with the specified dimension increased by the provided value.
+
+// The syntax [dimension] in the new object literal is called a computed property name. When an expression (in square brackets) is used in an object property name, the expression is computed, and the result is used as the property name. In this case, if dimension is "width", then [dimension]: box[dimension] + value would be equivalent to width: box.width + value. If dimension is "height", it would be equivalent to height: box.height + value.
+
+
+//   module.exports = { createBox, calculateArea, increment, increaseWidth, increaseHeight };
+  module.exports = { createBox, calculateArea, increment};
+
+  
 
 
 
--- test/box-test.js
+++ test/box-test.js
@@ -1,4 +1,84 @@
-// const chai = require('chai');
-// const expect = chai.expect;
-// const {createBox, calculateArea} = require('../box');
+// Import the necessary functions
+
+// Note
+// Pay close attention to all these imports - they may not always already be there for you.
+
+const chai = require('chai');
+// const chai = require('chai'); -> gives you access to the Chai assertion library.
+// const { expect } = require('chai');
+const expect = chai.expect;
+// const expect = chai.expect; -> gives you access to the Expect syntax from the Chai assertion library.
+// const {createBox, calculateArea, increment, increaseWidth, increaseHeight} = require('../box');
+const {createBox, calculateArea, increment} = require('../box');
+// const {createBox, calculateArea} = require('../box'); -> imports your functions from your box file into your test file. Remember this only works if you are also exporting each function from your box file.
+
+describe('Box functions', function() {
+    it('should return true', function() {
+        expect(true).to.deep.equal(true);
+        // expect(true).to.equal(Parvin);
+    });
+  describe('createBox', function() {
+   
+    it('should return a box with default dimensions when no arguments are passed', function() {
+      const box = createBox();
+      expect(box).to.deep.equal({height: 100, width: 100});
+    });
+
+    it('should return a box with specified dimensions when arguments are passed', function() {
+      const box = createBox(50, 70);
+      expect(box).to.deep.equal({height: 50, width: 70});
+    //   expect(box.height).to.equal(50);
+    //   expect(box.width).to.equal(70);
+    });
+
+  });
+
+  describe('calculateArea', function() {
+
+    it('should return the area of the box', function() {
+      const box = createBox(50, 70);
+      const area = calculateArea(box);
+      expect(area).to.equal(50 * 70);
+    });
+  });
+
+//   describe('increaseWidth', function() {
+
+//     it('should increase the width of the box by a provided value', function() {
+//       let box = createBox(50, 70);
+//       box = increaseWidth(box, 10);
+//       expect(box).to.deep.equal({height: 50, width: 80});
+//     });
+
+//   });
+
+//   describe('increaseHeight', function() {
+
+//     it('should increase the height of the box by a provided value', function() {
+//       let box = createBox(50, 70);
+//       box = increaseHeight(box, 10);
+//       expect(box).to.deep.equal({height: 60, width: 70});
+//     });
+
+//   });
+
+describe('increment', function() {
+    
+// create the tests for the new increment function. We will test if it increases the width and height of the box correctly:
+
+    it('should increase the width of the box by a provided value', function() {
+      let box = createBox(50, 70);
+      box = increment(box, 10, 'width');
+      expect(box).to.deep.equal({height: 50, width: 80});
+    });
+
+    it('should increase the height of the box by a provided value', function() {
+      let box = createBox(50, 70);
+      box = increment(box, 10, 'height');
+      expect(box).to.deep.equal({height: 60, width: 70});
+    });
+
+  });
+
+});
 
